d1c25dbd3e2a2311a2ed83f1ba116e6fa24b49f2
Aaron Patterson <aaron.patterson@gmail.com>
2010-04-04 21:08:35 -0700

    using a test config file

--- test/test_session.rb
+++ test/test_session.rb
@@ -1,13 +1,12 @@
 require "test/unit"
 require "mallcop"
+require 'psych'
 
 class TestSession < Test::Unit::TestCase
   def setup
     @c = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
     @c.connect(Socket.sockaddr_in(22, '127.0.0.1'))
-    @config = {
-      :username => 'apatterson'
-    }
+    @config = Psych.load_file File.join ENV['HOME'], '.mallcop_test'
   end
 
   def test_session
@@ -29,7 +28,7 @@ class TestSession < Test::Unit::TestCase
   def test_userauth_list
     session = MallCop::Session.new @c
     session.start
-    list = session.authlist_for @config[:username]
+    list = session.authlist_for @config['username']
     assert_instance_of Array, list
     assert list.include?('publickey')
   end
