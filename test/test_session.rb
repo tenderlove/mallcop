97ed03ad3b971225c77aa124f86024e010fd6145
Aaron Patterson <aaron.patterson@gmail.com>
2010-04-04 21:52:17 -0700

    test cleanup

--- test/test_session.rb
+++ test/test_session.rb
@@ -3,33 +3,27 @@ require "mallcop"
 require 'psych'
 
 class TestSession < MiniTest::Unit::TestCase
-  describe "a mallcop session" do
+  describe "a mallcop @session" do
     before do
       @c = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
       @c.connect(Socket.sockaddr_in(22, '127.0.0.1'))
       @config = Psych.load_file File.join ENV['HOME'], '.mallcop_test'
-    end
-
-    it "initializes" do
-      session = MallCop::Session.new @c
+      @session = MallCop::Session.new @c
     end
 
     it "starts" do
-      session = MallCop::Session.new @c
-      session.start
+      @session.start
     end
 
     it "provides a hostkey hash" do
-      session = MallCop::Session.new @c
-      session.start
-      hash = session.hostkey_hash MallCop::Session::HASH_SHA1
+      @session.start
+      hash = @session.hostkey_hash MallCop::Session::HASH_SHA1
       assert_instance_of String, hash
     end
 
     it "returns a list of authentication schemes" do
-      session = MallCop::Session.new @c
-      session.start
-      list = session.authlist_for @config['username']
+      @session.start
+      list = @session.authlist_for @config['username']
       assert_instance_of Array, list
       assert list.include?('publickey')
     end
@@ -39,11 +33,8 @@ class TestSession < MiniTest::Unit::TestCase
     #
     #  PasswordAuthentication yes
     it "logs in with username / password" do
-      session = MallCop::Session.new @c
-      session.start
-      hash = session.hostkey_hash MallCop::Session::HASH_SHA1
-      list = session.authlist_for @config['username']
-      assert session.userauth_password @config['username'], @config['password']
+      @session.start
+      assert @session.userauth_password @config['username'], @config['password']
     end
 
     ###
@@ -51,11 +42,8 @@ class TestSession < MiniTest::Unit::TestCase
     #
     #  PasswordAuthentication yes
     it "logs in fails with bad username / password" do
-      session = MallCop::Session.new @c
-      session.start
-      hash = session.hostkey_hash MallCop::Session::HASH_SHA1
-      list = session.authlist_for @config['username']
-      assert !session.userauth_password('foo', 'bar')
+      @session.start
+      assert !@session.userauth_password('foo', 'bar')
     end
   end
 end
