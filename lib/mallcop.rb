3a9d4cf9700bb4fbafda5656b5227e2036088b7b
Carl Lerche <carllerche@mac.com>
2010-04-08 21:16:07 -0700

    Add MallCop.connect that returns a connected session

--- lib/mallcop.rb
+++ lib/mallcop.rb
@@ -6,10 +6,16 @@ require 'mallcop/shell'
 module MallCop
   VERSION = '1.0.0'
 
-  def self.shell(host, username, options = {})
+  def self.connect(host, username, options = {})
     session = Session.start(host, options[:port] || 22)
     session.userauth_password username, options[:password]
 
+    yield session if block_given?
+    session
+  end
+
+  def self.shell(host, username, options = {})
+    session = connect(host, username, options)
     yield Shell.new(session)
   end
 end
